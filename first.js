048b1a1d1afc4dfd641951fa473db13edc855539
GermanAdutskevich <aduckevich.german@gmail.com>
2021-10-20 22:36:14 +0200

    Second day

--- first.js
+++ first.js
@@ -1,46 +1,34 @@
-//exersice 1
+// exercise 1
 
-
-if ((age = Number(prompt("Please, tell me your age")))) {
-  let sum = age + 1;
-  alert("Twoj wiek" + " " + sum);
-} else {
-  alert(":(");
-}
-
-//exersice 2
-
-let a = Number(prompt("First number:"));
-let b = Number(prompt("Second number:"));
-alert(a + b);
-
-//exersice 3
-
-let kilometers = Number(prompt("How much kilometers your run today?"));
-if (kilometers <= 3) {
-  alert("not good");
-} else if (kilometers <= 20) {
-  alert("GOOD");
-} else {
-  alert("marafon!!");
-}
-
-//exersice 4
-
-let floor = Number(prompt("Floor number"));
-
-if (floor < 1 || floor > 9) {
-  alert("Wrong number");
-}
-if (floor <= 3 || floor >= 7) {
-  alert("Ok");
-} else {
-  let password = prompt("Please, enter pin");
-  if (password == 1337) {
-    alert("Ok");
+function setAlarm(employed, vacation) {
+  let result;
+  if (employed === true && vacation === false) {
+    result = true;
   } else {
-    alert("WRONG");
+    result = false;
+  }
+
+  alert(result);
+}
+
+setAlarm(true, false);
+
+// exercise 2
+
+function YourFutureAge(BirthYear, AnotherYear) {
+  let age = AnotherYear - BirthYear;
+  if (age > 1) {
+    alert("You are" + " " + age + " " + "years old");
+  } else if (age == 0) {
+    alert("You were born this very year!");
+  } else {
+    age = BirthYear - AnotherYear;
+    if (age > 1) {
+      alert("You will be born in" + " " + age + " " + "years");
+    } else {
+      alert("You will be born in 1 year");
+    }
   }
 }
 
-
+YourFutureAge(2005, 2025);
